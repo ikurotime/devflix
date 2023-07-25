5c4571a8cb765d614e65eb4244a818ff44391df1
David Huertas <davidhuertasortiz@gmail.com>
2023-07-25 19:47:17 +0200

    first commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,20 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik'
+import type { DocumentHead } from '@builder.io/qwik-city'
 
 export default component$(() => {
   return (
     <>
       <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
     </>
-  );
-});
+  )
+})
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: 'Devflix',
   meta: [
     {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-};
+      name: 'description',
+      content: 'Devflix - A Netflix clone for developers'
+    }
+  ]
+}
